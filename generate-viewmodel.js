4b07a302e972cfd8df5594556a5ec1c9196f062f
itegrootenhuis <itegrootenhuis@bizstream.com>
2018-11-26 15:48:19 -0500

    In progress creating file at given file location

--- generate-viewmodel.js
+++ generate-viewmodel.js
@@ -1,6 +1,7 @@
 var prompt = require('prompt');
 var createFile = require('create-file');
 var find = require('find');
+var forEach = require('async-foreach').forEach;
 
 var schema = {
   properties: {
@@ -27,29 +28,41 @@ var schema = {
     //
     // Log the results.
     //
-    console.log('Command-line input received:');
-    console.log('  prefix: ' + result.prefix);
-    console.log('  File Name will be : ' + result.prefix+'ViewModel');
-
+    // console.log('Command-line input received:');
+    // console.log('  prefix: ' + result.prefix);
+    // console.log('  File Name will be : ' + result.prefix+'ViewModel');
+    
     generateFile(getViewModelPath(result.projectPath), result.prefix+'ViewModel.cs');
   });
 
 
   getViewModelPath = function (projectPath) { 
-    find.file(projectPath, function(files){
-      // console.log(files)
-      files.forEach(file => {
-        console.log('in loop ', file)
-        if(file.includes('*.Web\\ViewModels\\')){
-          console.log('condition met ', file);
+    console.log('***PATH***')
+    var viewModelFolderPath;
+    var regex = /^(.*[\\\/])/;
+
+    find.file(projectPath, function(files) {
+      forEach(files, function(file){
+        console.log(file)
+        if(file.includes('.Web\\ViewModels\\')) {
+          console.log('made it', file)
+          viewModelFolderPath = file.match(regex).input;
+          console.log('path', viewModelFolderPath)
         }
-      });
+      })
+
+      if(viewModelFolderPath != null){
+        console.log('return')
+        return viewModelFolderPath
+      }
     });
+    // return 'C:\\Users\\itegrootenhuis\\Documents\\BZS\\npm-test-project\\Test.Web\\ViewModels'
   }
 
 
   generateFile = function (path, fileName) {
-    console.log('genfile')
+    console.log('***GEN FILE***')
+    console.log(path, fileName)
     // createFile(path +'/'+ fileName, '', function(err){
       
     // });
